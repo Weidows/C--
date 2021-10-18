23d018335aef24cd1e84276498a136741366f2e5
Weidows <utsuko27@qq.com>
2021-10-18 22:27:45 +0800

    ✔ backup.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*
+ * @?: *********************************************************************
+ * @Author: Weidows
+ * @Date: 2021-10-18 19:54:50
+ * @LastEditors: Weidows
+ * @LastEditTime: 2021-10-18 19:57:55
+ * @FilePath: \C++\main.cpp
+ * @Description:
+ * @!: *********************************************************************
+ */
+#include <iostream>
+
+int main(int, char**) {
+    std::cout << "Hello, world!\n";
+}
